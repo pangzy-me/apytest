3dd07d82ffb27a2d5433cdaf90e4166916c667cf
pangzy <pangzy_me@163.com>
2019-07-05 10:59:18 +0800

    py文件开头注释使用

---
+++ headNotes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/5 10:51
+# @Author  : Author
+# @Site    : 
+# @File    : headNotes.py
+# @Software: PyCharm
+# @license : Copyright(C), xxx
+# @Contact : XXXXXX@163.com
+# @Desc    : some...
+
+print('py文件开头注释使用')
+'''
+    pyCharm设置路径：
+    File->settings->Editor->File and Code Templates->Python Script
+    添加代码如下：
+        #!/usr/bin/env python
+        # -*- coding: utf-8 -*-
+        # @Time    : ${DATE} ${TIME}
+        # @Author  : Aries
+        # @Site    : ${SITE}
+        # @File    : ${NAME}.py
+        # @Software: ${PRODUCT_NAME}
+        # @license : Copyright(C), Your Company
+        # @Contact : XXXXXX@gmail.com
+        # @Desc    :
+
+    参考文章：
+    https://segmentfault.com/q/1010000004878716
+    
+
+'''
